99105f7f348c0535049d79d8df5e7524b33bd849
Tim Brown <tim@redpill.me.uk>
2017-02-17 21:07:21 +0000

    modify brightness range (1-9) was (0-9)

--- python/blend.py
+++ python/blend.py
@@ -2,10 +2,10 @@ from microbit import *
 
 for x in range(0, 5):
     for y in range(0, 5):
-        display.set_pixel(x, y, (x+y+1) % 10)
+        display.set_pixel(x, y, x+y % 9 +1)
 
 while(True):
     sleep(500)
     for x in range(0, 5):
         for y in range(0, 5):
-            display.set_pixel(x, y, (display.get_pixel(x, y) + 1) % 10)
+            display.set_pixel(x, y, display.get_pixel(x, y)  % 9 +1)
