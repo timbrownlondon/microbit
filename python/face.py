3bbfc17ac3d02c5ca44f3920f722e7a8e081048d
Tim Brown <tim@redpill.me.uk>
2017-02-18 18:27:12 +0000

    add face.py - happy icon on button press

---
+++ python/face.py
@@ -0,0 +1,12 @@
+from microbit import *
+
+while True:
+    if button_a.is_pressed():
+        display.show(Image.HAPPY)
+    elif button_b.is_pressed():
+        break
+    else:
+        display.show(Image.SAD)
+
+# display.clear()
+display.scroll(str(button_a.get_presses()))
