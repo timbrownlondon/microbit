8dd632a657bc7dc7ed6c1df9e9ab0302f02a9502
Tim Brown <tim@redpill.me.uk>
2017-02-18 07:41:00 +0000

    add hello.py - some speech; add sparkle.py - random led brightness

---
+++ python/hello.py
@@ -0,0 +1,24 @@
+from microbit import *
+
+import speech
+import random
+
+image_list = [Image.HAPPY,
+              Image.SAD,
+              Image.YES,
+              Image.NO,
+              Image.HEART,
+              Image.SMILE,
+              Image.CONFUSED,
+              Image.ANGRY,
+              Image.ASLEEP,
+              Image.SURPRISED,
+              Image.SILLY,
+              Image.FABULOUS,
+              Image.MEH]
+
+for p in [20, 50, 100]:
+    for name in ['Alison', 'Maddie', 'Millie', 'Isaac', 'Tim', 'Harley']:
+        display.show(random.choice(image_list))
+        speech.say('hello '+name, pitch=p, speed=100)
+        sleep(1000)
