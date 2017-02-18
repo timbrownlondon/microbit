56ba724c8ca61be071a2426862ef772fce0a0d82
Tim Brown <tim@redpill.me.uk>
2017-02-18 19:57:05 +0000

    sparkle.py - updates faster

--- python/sparkle.py
+++ python/sparkle.py
@@ -3,7 +3,7 @@ from microbit import *
 import random
 
 while(True):
-    sleep(500)
+    sleep(50)
     for x in range(0, 5):
         for y in range(0, 5):
             display.set_pixel(x, y, random.choice(range(1, 10)))
