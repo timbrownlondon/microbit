52973101c50e8a5db36f46b4ddd394ccef1d4abf
Tim Brown <tim@redpill.me.uk>
2017-04-02 08:52:57 +0100

    working version of rock-paper-scissors.py - although can miss messages and get out of sync with peer

--- python/rock-paper-scissors.py
+++ python/rock-paper-scissors.py
@@ -1,29 +1,72 @@
 from microbit import *
+import radio
+import random
+
+# setup
+radio.on()
+moves = ['R', 'P', 'S']
+choice = random.randint(0, 2)
+my_score = 0
+others_score = 0
 
 
-def read_buttons():
-    a = button_a.was_pressed()
-    b = button_b.was_pressed()
-    # scissors
-    if a and b:
-        return('S')
-    # rock
-    if a:
-        return('R')
-    # paper
-    if b:
-        return('P')
-    return ' '
+def get_others_move():
+    while(True):
+        message = radio.receive()
+        if message in moves:
+            display.show(Image.CHESSBOARD)
+            sleep(1000)
+            return message
 
 
-def count_down():
-    for n in ['3', '2', '1', '0']:
-        display.show(n)
+def first_player_won(my_choice, their_choice):
+    if my_choice == 'R' and their_choice == 'S':
+        return True
+    if my_choice == 'P' and their_choice == 'R':
+        return True
+    if my_choice == 'S' and their_choice == 'P':
+        return True
+    return False
+
+
+def show_scores():
+    scores = str(my_score) + '-' + str(others_score) + ' '
+    display.scroll(scores, delay=200)
+
+
+display.scroll('***')
+while True:
+    if button_a.was_pressed():
+        choice = (choice + 1) % len(moves)
+    display.show(moves[choice])
+    sleep(100)
+    if button_b.was_pressed():
+        display.show(Image.ARROW_N)
+        radio.send(moves[choice])
+        sleep(200)
+
+        others_move = get_others_move()
+
+        display.scroll(moves[choice] + '-' + others_move)
+
+        if first_player_won(moves[choice], others_move):
+            display.show(Image.HAPPY)
+            my_score += 1
+        elif first_player_won(others_move, moves[choice]):
+            display.show(Image.SAD)
+            others_score += 1
+        else:
+            display.show('-')
         sleep(1000)
 
+        while not button_a.was_pressed():
+            sleep(50)
+        show_scores()
+        display.scroll('***')
 
-while(True):
-    count_down()
-    my_go = read_buttons()
-    display.show(my_go)
-    sleep(3000)
+        # clear any presses ready for next turn
+        button_a.get_presses()
+        button_b.get_presses()
+        choice = random.randint(0, 2)
+
+
