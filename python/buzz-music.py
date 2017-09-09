006028141ed330628cc241e82407f034307a95c5
Tim Brown <tim@redpill.me.uk>
2017-09-09 18:49:36 +0100

    modify buzz-music.py to go up/down music scale

--- python/buzz-music.py
+++ python/buzz-music.py
@@ -1,7 +1,12 @@
 from microbit import *
 import music
 
+pitch = 440
+interval = 1.12245
 while True:
-    for pitch in range(1000, 200, -100):
-        music.pitch(pitch, 500, wait=True)
-    sleep(1000)
+    if button_a.was_pressed():
+        pitch *= interval
+        music.pitch(int(pitch), 300)
+    if button_b.was_pressed():
+        pitch /= interval
+        music.pitch(int(pitch), 300)
