e823f4ba7c0600222c676e3a1829c7677e05c8fb
Tim Brown <tim@redpill.me.uk>
2025-07-09 17:06:25 +0100

    updated names

--- python/hello.py
+++ python/hello.py
@@ -18,7 +18,7 @@ image_list = [Image.HAPPY,
               Image.MEH]
 
 for p in [20, 50, 100]:
-    for name in ['Alison', 'Maddie', 'Millie', 'Isaac', 'Tim', 'Harley']:
+    for name in ['Alison', 'Madi', 'Milli', 'Isaac', 'Tim', 'Harley']:
         display.show(random.choice(image_list))
         speech.say('hello '+name, pitch=p, speed=100)
         sleep(1000)
